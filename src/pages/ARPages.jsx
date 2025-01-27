f69b9d47d7a5576c901a572cb718277f1451951f
unknown <helyxrulk@gmail.com>
2025-01-27 20:25:35 +0700

    fixing hiro

--- src/pages/ARPages.jsx
+++ src/pages/ARPages.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect } from "react";
-import "aframe";
-import "ar.js";
+import React from "react";
+import Hiro from "../hiro/Hiro";
 
 function ARPages() {
-  useEffect(() => {
-    // Ensure AFRAME is properly initialized
-    if (window.AFRAME) {
-      console.log("AFRAME is ready!");
-    }
-  }, []);
-
   return (
-    <div style={{ width: "100%", height: "100vh", margin: 0, padding: 0 }}>
-      <a-scene
-        vr-mode-ui="enabled: false"
-        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
-        renderer="logarithmicDepthBuffer: true;"
-        embedded
-      >
-        {/* NFT Marker */}
-        <a-nft
-          type="nft"
-          url="assets/marker/default-marker" // Path to your NFT marker files (without extension)
-          smooth="true"
-          smoothCount="10"
-          smoothTolerance="0.01"
-          smoothThreshold="5"
-        >
-          {/* 3D Box */}
-          <a-box
-            scale="100 100 100" // Adjust scale as needed
-            position="0 0.5 0"
-            material="color: red;"
-          ></a-box>
-        </a-nft>
-
-        {/* Camera */}
-        <a-entity camera></a-entity>
-      </a-scene>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex justify-center">
+        <Hiro />
+      </div>
     </div>
   );
 }
