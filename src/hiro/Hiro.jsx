08aa0ab31e8f57ed5e80dbf205e9ff878218c8ae
unknown <helyxrulk@gmail.com>
2025-01-27 23:05:23 +0700

    fixing hiro

--- src/hiro/Hiro.jsx
+++ src/hiro/Hiro.jsx
@@ -1,61 +1,21 @@
-import React, { useEffect } from "react";
+import React, { Component } from "react";
+import "aframe";
+import "aframe-ar"; // Correct library for AR.js integration in A-Frame
 
-function Hiro() {
-  useEffect(() => {
-    const marker = document.querySelector("a-marker");
-    const box = document.querySelector("a-box");
-
-    // Named event handlers for adding/removing
-    const handleMarkerFound = () => {
-      console.log("Marker detected! Box should be visible.");
-      if (box) {
-        box.setAttribute("visible", "true");
-      }
-    };
-
-    const handleMarkerLost = () => {
-      console.log("Marker lost! Box is hidden.");
-      if (box) {
-        box.setAttribute("visible", "false");
-      }
-    };
-
-    if (marker) {
-      marker.addEventListener("markerFound", handleMarkerFound);
-      marker.addEventListener("markerLost", handleMarkerLost);
-    }
-
-    // Cleanup
-    return () => {
-      if (marker) {
-        marker.removeEventListener("markerFound", handleMarkerFound);
-        marker.removeEventListener("markerLost", handleMarkerLost);
-      }
-    };
-  }, []);
-
-  return (
-    <a-scene
-      embedded
-      vr-mode-ui="enabled: false"
-      arjs="sourceType: webcam; trackingMethod: best; debugUIEnabled: false;"
-    >
-      {/* Hiro Marker */}
-      <a-marker preset="hiro">
-        {/* Add a blue box with rotation animation */}
-        <a-box
-          position="100 150 0"
-          material="color: blue; opacity: 1;"
-          scale="1000 1000 1000"
-          visible="false" // Initially hidden
-          animation="property: rotation; to: 0 360 0; loop: true; dur: 5000"
-        ></a-box>
-      </a-marker>
-
-      {/* Camera */}
-      <a-entity camera position="0 0 0" look-controls></a-entity>
-    </a-scene>
-  );
+class Hiro extends Component {
+  render() {
+    return (
+      <a-scene embedded arjs>
+        {/* Marker with 'hiro' preset */}
+        <a-marker preset="hiro">
+          {/* A blue box inside the marker */}
+          <a-box position="0 0.5 0" color="blue"></a-box>
+        </a-marker>
+        {/* Camera entity */}
+        <a-entity camera></a-entity>
+      </a-scene>
+    );
+  }
 }
 
 export default Hiro;
--- src/pages/ARPages.jsx
+++ src/pages/ARPages.jsx
@@ -3,10 +3,9 @@ import Hiro from "../hiro/Hiro";
 
 function ARPages() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <div className="flex justify-center">
-        <Hiro />
-      </div>
+    <div className="">
+        <h1>this is AR</h1>
+      <Hiro />
     </div>
   );
 }
